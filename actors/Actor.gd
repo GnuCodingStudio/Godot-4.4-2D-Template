8cc8d16c6e65950ba4c95d6c955ff3c398a8f317
Olivier PEREZ <olivier@olivierperez.fr>
2024-08-08 23:40:35 +0200

    feat(actor): make actor movable and use the right animation

--- actors/Actor.gd
+++ actors/Actor.gd
@@ -1,13 +1,30 @@
-extends CharacterBody2D
+extends RigidBody2D
 class_name Actor
 
 @export var speed = 300.0
 
-var state := State.IDLE
+@onready var animated_sprite = %AnimatedSprite
+@onready var collision_shape = %CollisionShape
+
+
+var state := State.IDLE:
+	set(value):
+		if state != value:
+			state = value
+			_on_state_changed()
+
 var moving_direction := Vector2.ZERO:
 	set(value):
-		moving_direction = value
-var facing_direction := Vector2.DOWN
+		if moving_direction != value:
+			moving_direction = value
+			_on_moving_direction_changed()
+
+var facing_direction := Vector2.DOWN:
+	set(value):
+		if facing_direction != value:
+			facing_direction = value
+			_on_facing_direction_changed()
+
 
 enum State {
 	IDLE,
@@ -15,10 +32,47 @@ enum State {
 }
 
 
-func _ready():
-	moving_direction = Vector2.UP
-	prints("moving_direction", moving_direction)
+func _process(delta):
+	if moving_direction == Vector2.ZERO:
+		state = State.IDLE
+	else:
+		state = State.MOVING
 
 
 func _physics_process(delta):
-	move_and_slide()
+	move_and_collide(moving_direction * speed * delta)
+
+
+func _direction_name() -> String:
+	match facing_direction:
+		Vector2.UP: return "Up"
+		Vector2.DOWN: return "Down"
+		Vector2.LEFT: return "Left"
+		Vector2.RIGHT: return "Right"
+		_: return "Down"
+
+
+func _update_animation():
+	var state_name := ""
+	match state:
+		State.IDLE: state_name = "Idle"
+		State.MOVING: state_name = "Move"
+
+	var animation_name = state_name + _direction_name()
+	animated_sprite.play(animation_name)
+
+
+func _on_state_changed():
+	_update_animation()
+
+
+func _on_moving_direction_changed():
+	if moving_direction == Vector2.ZERO or moving_direction == facing_direction:
+		return
+
+	# TODO Handle diagonals
+	facing_direction = moving_direction
+
+
+func _on_facing_direction_changed():
+	_update_animation()
