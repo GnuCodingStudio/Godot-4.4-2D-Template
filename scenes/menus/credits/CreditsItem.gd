665c52e8699e4d19d22e2492fa10f5af85cdce67
Olivier PEREZ <olivier@olivierperez.fr>
2024-08-15 10:29:24 +0200

    feat(menu): add shader background to main menu

--- scenes/menus/credits/CreditsItem.gd
+++ scenes/menus/credits/CreditsItem.gd
@@ -17,4 +17,4 @@ func _ready():
 
 
 func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/menus/MainMenu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/main/MainMenu.tscn")
