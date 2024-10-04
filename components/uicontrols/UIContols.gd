b8edc72e1389bd671ebfd6f2fd1f664c7ebca0f1
Olivier PEREZ <olivier@olivierperez.fr>
2024-10-04 13:30:28 +0200

    feat(ui): play UI audio in Globals

--- components/uicontrols/UIContols.gd
+++ components/uicontrols/UIContols.gd
@@ -4,9 +4,6 @@ const UI_CLICK = preload("res://components/uicontrols/UI_Click.wav")
 const UI_HOVER = preload("res://components/uicontrols/UI_Hover.wav")
 
 
-@onready var audio_player: AudioStreamPlayer = %AudioPlayer
-
-
 func _ready() -> void:
 	for child in get_parent().get_children():
 		_bind_node(child)
@@ -23,16 +20,14 @@ func _bind_node(node: Node) -> void:
 
 
 func _bind_button(button: Button) -> void:
-	button.mouse_entered.connect(_play_sound.bind(UI_HOVER))
-	button.pressed.connect(_play_sound.bind(UI_CLICK))
+	button.mouse_entered.connect(UiAudio.play_ui.bind(UI_HOVER))
+	button.pressed.connect(UiAudio.play_ui.bind(UI_CLICK))
 
 
 func _bind_checkbox(checkbox: CheckBox) -> void:
-	checkbox.mouse_entered.connect(_play_sound.bind(UI_HOVER))
-	checkbox.pressed.connect(_play_sound.bind(UI_CLICK))
+	checkbox.mouse_entered.connect(UiAudio.play_ui.bind(UI_HOVER))
+	checkbox.pressed.connect(UiAudio.play_ui.bind(UI_CLICK))
 
 
-func _play_sound(stream: AudioStream) -> void:
-	audio_player.stop()
-	audio_player.stream = stream
-	audio_player.play()
+#func _play_sound(stream: AudioStream) -> void:
+#	UiAudio.play_ui(stream)
