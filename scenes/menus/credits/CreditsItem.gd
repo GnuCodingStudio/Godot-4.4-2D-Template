aaf3899c859ccb1c2ad4d8267b75b66638fa9cf3
Olivier PEREZ <olivier@olivierperez.fr>
2024-10-06 19:32:53 +0200

    feat(ui): play audio on buttons/checkboxes interactions (#3)

--- scenes/menus/credits/CreditsItem.gd
+++ scenes/menus/credits/CreditsItem.gd
@@ -14,7 +14,3 @@ func _ready():
 	titleLabel.text = tr("CREDITS_TITLE_N_AUTHOR").format({"title": title, "author": author})
 	linkButton.text = link_text if link_text != null else link
 	linkButton.uri = link
-
-
-func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/menus/main/MainMenu.tscn")
